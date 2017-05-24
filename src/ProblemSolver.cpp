3c52e98ca698a16e1ba121eaee8df87751a4756c
刘群 <qunliu@zyhx-group.com>
2017-05-24 16:33:25 +0800

    尝试修正边界测试用例错误
    当 a, b 字符串完全相同的的特殊情况

--- src/ProblemSolver.cpp
+++ src/ProblemSolver.cpp
@@ -105,8 +105,8 @@ string ProblemSolver::FindLongestCommonToken_Iteratively(string a, string b) {
 	p1 = pShorter->c_str();
 	p2 = pLonger->c_str();
 	while (resultLength > 0) {
-		for (size_t i = (pShorter->length() - resultLength); i > 0; i--) {
-			for (size_t j = (pLonger->length() - resultLength); j > 0; j--) {
+		for (size_t i = (pShorter->length() - resultLength); i >= 0; i--) {
+			for (size_t j = (pLonger->length() - resultLength); j >= 0; j--) {
 				if (strncmp(p1 + i, p2 + j, resultLength) == 0) {
 					result = pShorter->substr(i, resultLength);
 					return result;
